4437389de603c230f27e3f1efc099905227e8040
Larissa Soares <larisbrasiliense@gmail.com>
2022-02-20 15:29:32 -0300

    Academia

---
+++ Desafio1.java
@@ -0,0 +1,23 @@
+//Questão 1:
+ 
+import java.util.Scanner;
+
+Scanner entrada = new Scanner(System.in);
+
+List<String> degraus = new ArrayList<>();
+
+System.out.print("Digite a quantidade de vezes: ");
+
+int qtdDegraus = entrada.nextInt();
+
+for (int i = 0; i < qtdDegraus; i++) {
+
+degraus.add(" ".repeat(qtdDegraus - i) + "*".repeat(i + 1));
+
+}
+
+for (String d : degraus ) {
+
+System.out.println(d);
+
+}
---
+++ Desafio3.java
@@ -0,0 +1,15 @@
+//Questão 3:
+
+import java.util.Scanner;
+
+public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("n");
+        double n = scanner.nextDouble();
+
+        if ( n == 1) {
+            System.out.println("     *");
+        }
